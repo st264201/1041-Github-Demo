1a7bc0634771300560f4b2eca3491d15c57098fd
G.Jim <andyzero75@gmail.com>
2015-11-19 11:32:47 +0800

    second merge conflict

--- atsoul.js
+++ atsoul.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 console.log("Stupid ATSouL");
 console.log("what a one-red-like man")
-=======
-console.log("ATSouL123lhh");
-console.log("GJim is too young too simple!");
->>>>>>> 04ce136a60472a976638fab292a86feb6bbf340a
+console.log("Don't change the line")
