b1d505b35e22033d217393a808f6ba6c8b00ea18
ATSouL <lebronjames75319@gmail.com>
2015-11-19 11:28:10 +0800

    bugfixed: 	-delete words not true

--- atsoul.js
+++ atsoul.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-console.log("Stupid ATSouL");
-console.log("what a one-red-like man")
-=======
+
 console.log("ATSouL123lhh");
 console.log("GJim is too young too simple!");
->>>>>>> 04ce136a60472a976638fab292a86feb6bbf340a
+
